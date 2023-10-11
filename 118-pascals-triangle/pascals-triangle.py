898b86f944333f49a9fa5990dc9ae421aa6254d2
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2023-10-12 00:36:35 +0530

    Time: 45 ms (23.59%) | Memory: 16.2 MB (90.08%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        # k(i)(j) = k(i-1)(j-1) + k(i-1)j
+        triangle = []
+        if numRows <= 0:
+            return triangle
+        for i in range(numRows):
+            row = []
+            for j in range(i+1):
+                if j==0 or j==i:
+                    row.append(1)
+                else:
+                    row.append(triangle[i - 1] [j - 1] + triangle[i - 1] [j])
+            triangle.append(row)
+        return triangle
