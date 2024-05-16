a1cc7539a1022ce3cd73e0af5d65ec295d1885bf
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2024-05-16 22:18:19 +0530

    Time: 53 ms (26.43%) | Memory: 16.8 MB (7.19%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.py
@@ -0,0 +1,14 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        li = [["I", 1], ["IV", 4],["V", 5], ["IX", 9], ["X", 10], ["XL", 40], ["L", 50], ["XC", 90], ["C",100],["CD",400], ["D",500], ["CM",900], ["M",1000]]
+        res =""
+        
+        for sym, n in reversed(li):
+            if num // n:
+                count = num//n
+                print(count)
+                res+= (sym * count)
+                print(res)
+                num = num % n
+        return res
+                
