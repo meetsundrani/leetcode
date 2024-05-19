37d25ebf6b0239121031614719a709563e11c69f
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2024-05-19 14:37:02 +0530

    Time: 129 ms (46.91%) | Memory: 18.1 MB (31.73%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -5,6 +5,8 @@ class Solution:
         """
         j = 0
         for i in range(len(nums)):
-            if nums[i]!=0:
+            if nums[i]!= 0:
                 nums[j], nums[i] = nums[i], nums[j]
                 j+=1
+                
+        
