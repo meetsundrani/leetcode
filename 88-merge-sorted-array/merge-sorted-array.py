06eb43840aaa0ec65fa08b800550860eabbd56b9
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2024-05-19 14:29:20 +0530

    Time: 40 ms (52.90%) | Memory: 16.5 MB (78.42%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -6,7 +6,6 @@ class Solution:
         i = m-1
         j = n-1
         k = m+n-1
-        
         while j>=0:
             if i>=0 and nums1[i] > nums2[j]:
                 nums1[k] = nums1[i]
@@ -15,3 +14,4 @@ class Solution:
                 nums1[k] = nums2[j]
                 j-=1
             k-=1
+        
