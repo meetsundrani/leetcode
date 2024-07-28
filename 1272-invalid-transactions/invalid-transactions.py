3c4df80ced009011e738bae8a3ca29addc9e69d8
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2024-07-28 17:05:17 +0530

    Time: 61 ms (73.98%) | Memory: 17.3 MB (25.51%) - LeetSync

--- 1272-invalid-transactions/invalid-transactions.py
+++ 1272-invalid-transactions/invalid-transactions.py
@@ -1,24 +1,49 @@
-class Solution:
+class Solution(object):
     def invalidTransactions(self, transactions: List[str]) -> List[str]:
-        ret = set()
-        dic = collections.defaultdict(list)
-        for i,item in enumerate(transactions):
-            name, time, amount, city = item.split(",")
-            dic[name].append((int(time),city,i))
-            if int(amount) > 1000:
-                ret.add(i)
-        for k,v in dic.items():
-            v.sort()
-            vleng = len(v)
-            for i in range(vleng):
-                old = v[i]
-                for j in range(i+1,vleng):
-                    new = v[j]
-                    diff = new[0]-old[0]
-                    if diff <= 60:
-                        if new[1] != old[1]:
-                            ret.add(old[2])
-                            ret.add(new[2])
-                    else:
-                        break
-        return [transactions[x] for x in ret]
+        
+        # make a defaultdict to save the transactions first
+        mapped_transactions = collections.defaultdict(lambda: collections.defaultdict(set))
+
+        # go through the transactions and save all of them in the structure    
+        for transaction in transactions:
+            name, time, amount, city = Solution.parse_transaction(transaction)
+            mapped_transactions[name][time].add(city)
+        print(mapped_transactions)
+        
+        # go through the transactions again and check for invalid ones
+        result = []
+        for transaction in transactions:
+
+            # parse the transcation
+            name, time, amount, city = Solution.parse_transaction(transaction)
+            
+            # check for the amount
+            if amount > 1000:
+                result.append(transaction)
+                continue
+            
+            # check whether we have a transaction with the same name
+            if name not in mapped_transactions:
+                continue
+            
+            # check if other transaction exist within the invalid timeframe
+            for time_point in range(time-60, time+61):
+
+                # time point does not exist
+                if time_point not in mapped_transactions[name]:
+                    continue
+
+                # check if there are more cities in within the time frame and the name
+                # and if it is only one check that it is not the same city!
+                if len(mapped_transactions[name][time_point]) > 1 or (city not in mapped_transactions[name][time_point]):
+                    result.append(transaction)
+                    break
+                                        
+        return result
+    
+    @staticmethod
+    def parse_transaction(transaction: str) -> Tuple[str, int, int, str]:
+        name, time, amount, city = transaction.split(',')
+        time = int(time)
+        amount = int(amount)
+        return name, time, amount, city
