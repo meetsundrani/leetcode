02708ec8f5e75b8733296ea43a5cfa99b1f93bf9
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2025-06-21 00:12:28 +0530

    Time: 3 ms (63.74%) | Memory: 18.9 MB (18.33%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,7 +1,5 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        summ = 0
-        for i in range((len(nums) + 1)):
-            print(i)
-            summ += i
-        return (summ - sum(nums))
+        expected = sum(range(len(nums)+1))
+        actual = sum(nums)
+        return expected - actual
