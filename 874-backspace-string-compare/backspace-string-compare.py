9cb581de7dce15c189a7a1e3bf8a72054349be3b
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2025-06-30 23:26:01 +0530

    Time: 3 ms (13.78%) | Memory: 17.6 MB (86.39%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.py
+++ 874-backspace-string-compare/backspace-string-compare.py
@@ -1,5 +1,4 @@
 class Solution:
-    
     def backspaceCompare(self, s: str, t: str) -> bool:
         l1 = []
         l2 = []
@@ -7,7 +6,7 @@ class Solution:
             if i != '#':
                 l1.append(i)
             else:
-                if len(l1)!=0:
+                if len(l1) !=0:
                     l1.pop()
         for i in t:
             if i != '#':
@@ -15,11 +14,7 @@ class Solution:
             else:
                 if len(l2) !=0:
                     l2.pop()
-
-        if ''.join(l1) == ''.join(l2):
+        if ''.join(l1)==''.join(l2):
             return True
         else:
             return False
-            
-
-   
