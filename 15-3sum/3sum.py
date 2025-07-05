0b1e26f7fc6b8aae840032368f92446b80f74298
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2025-07-05 12:10:48 +0530

    Time: 580 ms (57.67%) | Memory: 20.7 MB (36.62%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,35 +1,32 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()  # Step 1: Sort the array
+        nums.sort()
         res = []
-        n = len(nums)
-        
-        for i in range(n):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue  # Skip duplicate fixed elements
-            
-            left = i + 1
-            right = n - 1
-            
-            while left < right:
-                total = nums[i] + nums[left] + nums[right]
-                
-                if total == 0:
-                    res.append([nums[i], nums[left], nums[right]])
-                    
-                    # Skip duplicates for left and right
-                    while left < right and nums[left] == nums[left + 1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
-                    
-                    left += 1
-                    right -= 1
-                    
-                elif total < 0:
-                    left += 1
-                else:
-                    right -= 1
-        
-        return res
 
+        n = len(nums)
+
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l = i+1
+            r = n-1
+
+            while l < r:
+                total = nums[i] + nums[l] + nums[r]
+                if total == 0:
+                    res.append([nums[i], nums[l], nums[r]])
+                
+                    while l < r and nums[l] == nums[l+1]:
+                        l+=1
+                    while l < r and nums[r] == nums[r-1]:
+                        r-=1
+
+                    l+=1
+                    r-=1
+
+                elif total < 0:
+                    l+=1
+                else:
+                    r-=1
+        return res
+                
