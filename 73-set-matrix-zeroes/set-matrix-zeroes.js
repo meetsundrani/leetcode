938dc9d4a7d940b796ade96f666ba757e461361f
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2025-08-02 11:57:30 +0530

    Time: 0 ms (100.00%) | Memory: 57.8 MB (95.50%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var setZeroes = function(matrix) {
+    const n = matrix.length;
+    const m = matrix[0].length;
+    const row = new Array(n).fill(0); // row array
+    const col = new Array(m).fill(0); 
+    for(let i=0;i<n;i++){
+        for(let j=0;j<m;j++){
+            if(matrix[i][j] ===0){
+            row[i] = 1;
+            col[j]=1;
+            }
+        }
+    }
+    // Finally, mark all (i, j) as 0
+    // if row[i] or col[j] is marked with 1.
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < m; j++) {
+            if (row[i] || col[j]) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+};
