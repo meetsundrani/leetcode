c33c9690e480e479df9a285ba7cccba42bfe22c0
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2025-08-05 01:03:42 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (97.74%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,20 +4,13 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         i = j = len(nums)-1
-        while i > 0 and nums[i-1] >= nums[i]:
-            i -= 1
-        if i == 0:
+        while i>0 and nums[i-1] >= nums[i]:
+            i-=1
+        if i==0:
             nums.reverse()
-            return 
-        # 2. If it's not zero then we'll find the first number grater then nums[i-1] starting from end
+            return
+        
         while nums[j] <= nums[i-1]:
-            j -= 1
-        # Now out pointer is pointing at two different positions
-        # i. first non-assending number from end
-        # j. first number greater than nums[i-1]
-        
-        # We'll swap these two numbers
+            j-=1
         nums[i-1], nums[j] = nums[j], nums[i-1]
-        
-        # We'll reverse a sequence strating from i to end
-        nums[i:]= nums[len(nums)-1:i-1:-1]
+        nums[i:] = nums[len(nums)-1:i-1:-1]
