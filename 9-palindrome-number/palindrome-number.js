cf6e726a4cbb13377099368d8c98f824b899fac8
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2026-02-22 21:26:31 +0530

    Time: 15 ms (12.25%) | Memory: 63.8 MB (65.02%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,15 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    while(x < 0) {
+     if (x < 0 || (x % 10 === 0 && x !== 0)) {
         return false;
     }
-  let og = x;
-  let rev = 0;
-  while (x > 0) {
-    rev = rev * 10 + x%10;
-    x = Math.floor(x/10);
-  }
 
-  return og === rev;
+    let reversedHalf = 0;
+
+    while (x > reversedHalf) {
+        reversedHalf = reversedHalf * 10 + (x % 10);
+        x = Math.floor(x / 10);
+    }
+
+    return x === reversedHalf || x === Math.floor(reversedHalf / 10);
 };
