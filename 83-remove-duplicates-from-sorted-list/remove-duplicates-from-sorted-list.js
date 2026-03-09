cfedc6e3c5765fa604ad9547b8716979edb88dca
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2026-03-10 00:29:57 +0530

    Time: 1 ms (39.84%) | Memory: 57.5 MB (80.80%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.js
@@ -10,12 +10,12 @@
  * @return {ListNode}
  */
 var deleteDuplicates = function(head) {
-    let curr = head;
-    while(curr != null && curr.next != null){
-        if(curr.val == curr.next.val){
-            curr.next = curr.next.next;
+    let temp = head;
+    while(temp != null && temp.next != null){
+        if(temp.val == temp.next.val){
+            temp.next = temp.next.next;
         } else {
-            curr = curr.next;
+            temp = temp.next;
         }
     }
     return head;
