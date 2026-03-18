1c9b86021ec96b32985e90b951ed2f568abe65d9
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2026-03-19 00:13:16 +0530

    Time: 0 ms (100.00%) | Memory: 54.8 MB (11.63%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.js
+++ 58-length-of-last-word/length-of-last-word.js
@@ -5,12 +5,11 @@
 var lengthOfLastWord = function(s) {
     let i = s.length - 1;
     let length = 0;
-    while(s[i] === " ") i-=1
+while(s[i] === " ") i-=1;
 
-    while(i >=0 && s[i] !== " "){
-length++;
-i-=1;
-    }
+while(i >= 0 && s[i] !== " "){
+    length++;
+    i-=1;
+}
     return length;
-
 };
