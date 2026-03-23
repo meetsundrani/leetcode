8c8764f311c53c79b824795dc18349ff730559c5
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2026-03-24 00:38:38 +0530

    Time: 5 ms (65.05%) | Memory: 57.9 MB (61.33%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -3,15 +3,7 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-        let str = s.split(",").join("")
-    let sr = str.split(" ").join("")
-    let string = sr.toLowerCase().split(/[^a-z0-9]+/).join("");
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] !== string[string.length - 1 - i]) {
-            return false
-        }
-    }
-    return true
-
-
+  let str = s.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+  let revStr = str.split('').reverse().join('');
+  return revStr === str;
 };
