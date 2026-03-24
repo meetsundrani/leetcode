dc8ab7dc618d9d3e63c4a221c62131911b796a24
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2026-03-25 00:36:38 +0530

    Time: 3 ms (99.01%) | Memory: 54.4 MB (96.87%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,9 +5,15 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
-    let sorted1 =s.split('').sort().join('');
-    let sorted2 =t.split('').sort().join('');
-    console.log(sorted1, sorted2)
-return sorted1 === sorted2
+  const arr = new Array(26).fill(0)
 
+    for (let i = 0; i < s.length; i++) {
+        arr[s.charCodeAt(i) - 97] += 1
+        arr[t.charCodeAt(i) - 97] -= 1
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (arr[i] !== 0) return false
+    }
+    return true
 };
