4182bd4056d037016929e080eaab7519a74a7f6d
Meet Sundrani <41486000+meetsundrani@users.noreply.github.com>
2026-03-27 21:04:28 +0530

    Time: 4 ms (98.15%) | Memory: 54.3 MB (97.33%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -5,15 +5,16 @@
  */
 var isAnagram = function(s, t) {
     if(s.length !== t.length) return false;
-  const arr = new Array(26).fill(0)
+    let arr = new Array(26).fill(0);
 
-    for (let i = 0; i < s.length; i++) {
-        arr[s.charCodeAt(i) - 97] += 1
-        arr[t.charCodeAt(i) - 97] -= 1
+for(let i=0; i<s.length; i++){
+    arr[s.charCodeAt(i) - 97] += 1;
+    arr[t.charCodeAt(i) - 97] -= 1;
     }
-
-    for (let i = 0; i < 26; i++) {
-        if (arr[i] !== 0) return false
+    for(let i=0;i< 26; i++){
+        if(arr[i] !== 0){
+            return false;
+        }
     }
-    return true
+return true;
 };
